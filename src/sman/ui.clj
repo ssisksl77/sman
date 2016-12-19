b85de633866b7837bddf0674a551a70d544673f4
Younghwan Nam <ssisksl77@gmail.com>
2016-12-19 23:52:24 +0900

    making component... really awkward...

--- src/sman/ui.clj
+++ src/sman/ui.clj
@@ -2,8 +2,20 @@
     (:require [seesaw.core :as ss]))
 
 (ss/native!)
-(println "native!")
 
-(def f (ss/frame :title "My First Clojure Program!!! with Swing!!! fantastic!!"))
+(def f (ss/frame
+         :title "switch into csv for email. deadliy useful."
+         :size [500 :by 300]
+         :on-close :exit))
+
+(defn display [content]
+  (ss/config! f :content content) content)
+
+
+(display (ss/border-panel
+          :north (ss/horizontal-panel :items ["name: " (ss/label :text "Younghwan Name") "   email: " (ss/label :text "ssisksl77@gmail.com")])
+          :center (ss/left-right-split (ss/scrollable (ss/button :text "switch -> csv")) (ss/scrollable (ss/text :multi-line? true :text "right-label\nhello world")))
+           ))
+
 
 (defn go! [] (-> f ss/pack! ss/show!))
