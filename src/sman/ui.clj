63f114713aaf4c49a2f2f64f3244fe23cdf7661e
Younghwan Nam <ssisksl77@gmail.com>
2016-12-21 00:10:36 +0900

    이메일만 필터링 되도록

--- src/sman/ui.clj
+++ src/sman/ui.clj
@@ -1,23 +1,33 @@
 (ns sman.ui
-    (:require [seesaw.core :as ss]))
+    (:require [seesaw.core :as ss]
+              [seesaw.chooser :as choose]))
 
 (ss/native!)
+
+(defn display
+  ([content] (ss/config! f :content content) content)
+  ([where content] (ss/config! where :text content)))
 
 (def f (ss/frame
          :title "MAGIC STRING"
          :minimum-size [500 :by 300]
-         :on-close :exit
+         ;:on-close :exit
          :icon (clojure.java.io/resource "yhnam.png")))
 
-(defn display [content]
-  (ss/config! f :content content) content)
+(def c-pan-textarea (ss/text :multi-line? true :text ""))
+
 
 (def n-pan (ss/horizontal-panel :items
   ["name: " (ss/label :text "Younghwan Name")
    "   email: " (ss/label :text "ssisksl77@gmail.com")
-   (ss/button :text "open .txt")]))
+   (ss/button :text "open.txt" :listen [:action (fn [event] (choose/choose-file :success-fn (fn [fc file] (display c-pan-textarea file))))])]))
 
-(def c-pan (ss/left-right-split (ss/scrollable (ss/button :text "get email")) (ss/scrollable (ss/text :multi-line? true :text ""))))
+(def c-pan (ss/left-right-split
+  (ss/scrollable (ss/button
+                  :text "get email"
+                  :listen [:action (fn [event] (display c-pan-textarea
+                    (reduce str (interpose ", " (filter #(re-matches #".+\@.+\..+" %) (clojure.string/split (ss/text c-pan-textarea) #"\n"))))))]))
+  (ss/scrollable c-pan-textarea )))
 
 (def s-pan "south")
 
@@ -25,5 +35,4 @@
           :north n-pan
           :center c-pan
           :south s-pan ))
-
 (defn go! [] (-> f ss/pack! ss/show!))
